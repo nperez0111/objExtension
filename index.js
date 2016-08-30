0a189767170a9ec8d9ff011868af7d8bb7e95343
Nick The Sick <nperez0111@gmail.com>
2016-08-30 09:57:44 -0700

    added many tests and methods

--- index.js
+++ index.js
@@ -1,10 +1,158 @@
 'use strict';
-module.exports = (input, opts) => {
-	if (typeof input !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof input}`);
-	}
+const extend = require( 'objextender' ),
+    merge = require( 'merge' ),
+    isObj = require( 'is-object' ),
+    addDiff = function ( that, obj ) {
+        Object.keys( obj ).forEach( function ( cur ) {
+            if ( isObj( obj[ cur ] ) ) {
+                if ( !that.hasOwnProperty( cur ) || !isObj( that[ cur ] ) ) {
+                    that[ cur ] = {};
+                }
+                addDiff( that[ cur ], obj[ cur ] );
+            } else if ( !that.hasOwnProperty( cur ) ) {
 
-	opts = opts || {};
+                that[ cur ] = obj[ cur ];
+            }
+        } );
+    },
+    makeObj = function ( keys, values ) {
+        let ret = {}
 
-	return input + ' & ' + (opts.postfix || 'rainbows');
+        if ( Array.isArray( keys ) && Array.isArray( values ) ) {
+
+            keys.forEach( ( cur, i ) => {
+
+                ret[ cur ] = values[ i ]
+
+            } )
+
+        } else {
+
+            ret[ keys ] = values
+
+        }
+
+        return ret
+
+    },
+    allOptions = {
+        pick: function ( get, prop ) {
+            let obj = get()
+
+            if ( Array.isArray( prop ) ) {
+
+                let ret = {};
+                prop.forEach( ( cur ) => {
+
+                    ret[ cur ] = allOptions.pick( get, cur )
+
+                } )
+
+                return ret
+
+            }
+            if ( !obj.hasOwnProperty( prop ) ) {
+
+                throw Error( `Object does not have property '${cur}'` )
+
+            }
+            return obj[ prop ];
+        },
+        keys: function ( get ) {
+            return Object.keys( get() )
+        },
+        values: function ( get ) {
+            return allOptions.keys( get ).map( key => get()[ key ] )
+        },
+        forEach: function ( get, doThat ) {
+
+            let obj = get()
+
+            return allOptions.keys( get ).forEach( function ( cur ) {
+
+                doThat.call( obj, obj[ cur ], cur, obj )
+
+            } )
+
+        },
+        map: function ( get, doThat ) {
+
+            let obj = get(),
+                keys = Object.keys( obj )
+
+            return makeObj( keys, keys.map( function ( cur, i ) {
+
+                return doThat.call( obj, obj[ cur ], cur, obj )
+
+            } ) )
+
+        },
+        every: function ( get, check ) {
+            return allOptions.values( get ).every( check )
+        },
+        none: function ( get, check ) {
+            return !allOptions.every( get, check )
+        },
+        some: function ( get, check ) {
+            return allOptions.values( get ).some( check )
+        },
+        filter: function ( get, check ) {
+            let keys = allOptions.keys( get ),
+                temp = [
+                    [],
+                    []
+                ],
+                vals = allOptions.values( get ),
+                indexThatPassed = vals.forEach( function ( cur, i, arr ) {
+
+                    if ( check.call( get(), cur, i, arr ) ) {
+
+                        temp[ 0 ].push( keys[ i ] )
+                        temp[ 1 ].push( cur )
+
+                    }
+
+                } );
+
+            return makeObj( temp[ 0 ], temp[ 1 ] )
+
+        },
+        keyOf: function ( get, query ) {
+            let index = allOptions.values( get ).indexOf( query )
+            if ( index === -1 ) {
+                return false;
+            }
+            return allOptions.keys( get )[ index ];
+        },
+        includes: function ( get, query ) {
+            return allOptions.keyOf( get, query ) === false ? false : true;
+        },
+        copy: function ( get ) {
+            return makeObj( allOptions.keys( get ), allOptions.values( get ) )
+        },
+        remove: function ( get, key ) {
+            if ( Array.isArray( key ) ) {
+                return key.map( ( c ) => {
+                    return allOptions.remove.call( this, get, c )
+                } )
+            }
+            if ( this.hasOwnProperty( key ) ) {
+                let val = this[ key ]
+                delete this[ key ]
+                return val;
+            }
+            return false;
+        },
+        extend: function ( get ) {
+            return merge.recursive.apply( this, [ true ].concat( get() ).concat( Array.from( arguments ).slice( 1 ) ) );
+        },
+        assign: function ( get ) {
+            addDiff.call( this, this, allOptions.extend.apply( this, arguments ) )
+            return this;
+        }
+    }
+module.exports = ( opts ) => {
+    if ( opts === undefined ) {
+        return extend( allOptions );
+    }
 };
--- test.js
+++ test.js
@@ -1,6 +1,136 @@
 import test from 'ava';
 import fn from './';
+let please = fn(),
+    x = {
+        a: {
+            c: 1
+        },
+        b: 2,
+        d: true
+    }
 
-test('title', t => {
-	t.is(fn('unicorns'), 'unicorns & rainbows');
-});
+test( 'Pick Method', t => {
+    t.true( x[ please.pick ]( 'd' ) )
+    t.deepEqual( x[ please.pick ]( [ 'd', 'b' ] ), {
+        b: 2,
+        d: true
+    } )
+    t.throws( function () {
+        x[ please.pick ]( 'notExistent' )
+    } )
+} )
+
+test( 'Keys Method', t => {
+    t.deepEqual( x[ please.keys ](), [ 'a', 'b', 'd' ] )
+} )
+
+test( 'Values Method', t => {
+    t.deepEqual( x[ please.values ](), [ {
+        c: 1
+    }, 2, true ] )
+} )
+
+test( 'ForEach Method', t => {
+    var i = 0;
+    x[ please.forEach ]( function () {
+        i++
+    } )
+    t.deepEqual( i, 3 )
+} )
+
+test( 'Map Method', t => {
+    t.deepEqual( x[ please.map ]( function ( cur, key ) {
+        return key
+    } ), {
+        a: 'a',
+        b: 'b',
+        d: 'd'
+    } )
+} )
+
+test( 'Every Method', t => {
+    t.true( x[ please.every ]( function ( a ) {
+        return a === a;
+    } ) )
+    t.false( x[ please.every ]( function ( a ) {
+        return Number( a ) === a;
+    } ) )
+} )
+
+test( 'Some Method', t => {
+    t.true( x[ please.some ]( function ( a ) {
+        return Number( a ) === a
+    } ) )
+} )
+
+test( 'Filter Method', t => {
+    t.deepEqual( x[ please.filter ]( function ( a ) {
+        return Number( a ) === a
+    } ), {
+        b: 2
+    } )
+} )
+
+test( 'Keyof Method', t => {
+    t.deepEqual( x[ please.keyOf ]( true ), "d" )
+} )
+
+test( 'Includes Method', t => {
+    t.true( x[ please.includes ]( true ) )
+} )
+
+test( 'Copy Method', t => {
+    let y = {
+        s: 3
+    }
+    t.deepEqual( y[ please.copy ](), {
+        s: 3
+    } )
+} )
+
+test( 'Remove Method', t => {
+    let y = {
+        d: 34,
+        c: 35,
+        f: 34,
+        l: 25
+    }
+    t.is( y[ please.remove ]( "d" ), 34 )
+    t.deepEqual( y, {
+        c: 35,
+        f: 34,
+        l: 25
+    } )
+    t.deepEqual( y[ please.remove ]( [ "c", "f" ] ), [ 35, 34 ] )
+} )
+
+test( 'Extend Method', t => {
+    let y = {
+        d: 3
+    }
+    t.deepEqual( y[ please.extend ]( {
+        x: 3
+    } ), {
+        d: 3,
+        x: 3
+    } )
+    t.deepEqual( y, {
+        d: 3
+    } )
+} )
+
+test( 'Assign Method', t => {
+    let y = {
+        d: 3
+    }
+    t.deepEqual( y[ please.assign ]( {
+        x: 3
+    } ), {
+        d: 3,
+        x: 3
+    } )
+    t.deepEqual( y, {
+        d: 3,
+        x: 3
+    } )
+} )
